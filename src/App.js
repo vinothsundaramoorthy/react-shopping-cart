0ed890384ec3f23a66c777824432a4e4a39018f7
Vinoth S <vinoth.s@thinkpalm.com>
2021-09-19 13:20:24 +0530

    cart component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,42 @@ import React, { Component } from "react";
 import Products from "./components/Products";
 import Filter from "./components/Filter";
 import data from "./data.json";
+import Cart from "./components/Cart";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: "",
     };
   }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter(x => x._id !== product._id),
+    });  
+  }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if(item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart) {
+      cartItems.push({...product, count: 1});
+    }
+    this.setState({cartItems});
+  }
+
+
   sortProducts = (event) => {
     const sort = event.target.value;
     this.setState((state) => ({
@@ -64,9 +90,11 @@ class App extends Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products} />
+              <Products products={this.state.products} addToCart={this.addToCart} />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}/>
+            </div>
           </div>
         </main>
         <footer>All Right is reserved</footer>
---
+++ src/components/Cart.js
@@ -0,0 +1,54 @@
+import React, { Component, Fragment } from "react";
+import formatCurrency from "../util";
+
+export default class Cart extends Component {
+  render() {
+    const { cartItems } = this.props;
+    return (
+      <Fragment>
+        <div>
+          {cartItems.length === 0 ? (
+            <div className="cart cart-header">Cart is Empty</div>
+          ) : (
+            <div className="cart cart-header">
+              You have {cartItems.length} in your cart.
+            </div>
+          )}
+        </div>
+        <div className="cart">
+          <ul className="cart-items">
+            {cartItems.map((item) => (
+              <li key={item._id}>
+                <div>
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div>
+                  <div>{item.title}</div>
+                  <div className="right">
+                    {formatCurrency(item.price)} * {item.count}{" "}
+                    <button onClick={() => this.props.removeFromCart(item)}>
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {cartItems.length !== 0 && (
+          <div className="cart">
+            <div className="total">
+              <div>
+                Total:{" "}
+                {formatCurrency(
+                  cartItems.reduce((a, c) => a + c.price * c.count, 0)
+                )}
+              </div>
+              <button className="button primary">Proceed</button>
+            </div>
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+}
